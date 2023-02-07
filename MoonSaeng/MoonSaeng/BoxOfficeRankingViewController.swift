6cb0fb70607424bfcaafd5eee8d1296fbc778419
seohyeon2 <seo_hyeon418@naver.com>
2023-02-07 17:57:21 +0900

    feat: 스토리보드 지운 프로젝트 추가

---
+++ MoonSaeng/MoonSaeng/BoxOfficeRankingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BoxOfficeRankingViewController.swift
+//  MoonSaeng
+//
+//  Created by seohyeon park on 2023/02/07.
+//
+
+import UIKit
+
+class BoxOfficeRankingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
